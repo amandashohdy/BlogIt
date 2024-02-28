c37933c78230d46cd22405e964ce8a9dbc3232ff
amandashohdy <amandashohdy@gmail.com>
2024-02-27 16:36:57 -0800

    All Pages Navigate

--- BlogIt!/src/pages/LoginForm/LoginForm.jsx
+++ BlogIt!/src/pages/LoginForm/LoginForm.jsx
@@ -4,11 +4,35 @@ import { CiUser } from "react-icons/ci" ;
 import { CiLock } from "react-icons/ci" ; 
 import { Link } from 'react-router-dom' ;
 
+function Login() {
+  return (
+    <button className="Login">
+        <Link to="/profile"> Login </Link>
+    </button>
+  ) ;
+}
+
+function RegisterButton() {
+  return (
+      <button className="RegisterButton">
+        <Link to="/register"> Register </Link>
+    </button>
+  ) ;
+}
+
+function ForgotPassword() {
+  return (
+      <button className="ForgotPassword">
+        <Link to="/forgot_password"> Forgot password? </Link>
+      </button>
+  )
+}
+
 const LoginForm = () => {
   return (
     <div className='wrapper'>
       <form action="">
-          <h1>Login</h1>
+          <h1>Blog It!</h1>
           <div className="input-box">
               <input type="text" placeholder="Username" required></input>
               <CiUser className='icon'/>
@@ -21,15 +45,15 @@ const LoginForm = () => {
 
           <div className="remember-forgot">
             <label><input type="checkbox"/>Remember me</label>
-            <a href="#">Forgot password?</a>
+            <ForgotPassword/>
           </div>
 
-          <button type="submit">
-            <Link to="/profile"> Login </Link>
-          </button>
+          <div>
+            <Login/>
+          </div>
 
           <div className="register-link">
-              <p>Don't have an account? <a href="#">Register</a></p>
+              Don't have an account? <RegisterButton/>
           </div>
       </form>
     </div>
